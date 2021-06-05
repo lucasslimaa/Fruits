2ac5d0ba699da32458d78a3144b8591debd3bfb1
lucasslimaa <lucasanches22@gmail.com>
2021-06-05 01:00:18 -0300

    Create project structure

---
+++ src/Fruits.API/Fruits.API/WeatherForecast.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fruits.API.Controllers
+{
+    public class WeatherForecast
+    {
+        internal string Summary;
+
+        public DateTime Date { get; internal set; }
+        public int TemperatureC { get; internal set; }
+    }
+}
